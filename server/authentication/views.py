29190d4025e8d504069ee7a8bf11d96839cb3427
TheRedWizard <sreeraghavan2016@gmail.com>
2024-07-06 12:45:59 +0530

    changed registration flow

--- server/authentication/views.py
+++ server/authentication/views.py
@@ -2,6 +2,9 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from .models import User
 from pydantic import BaseModel
+from typing import Optional
+from evaluators_qpuploaders import views as eq_views
+from students_mentors import views as sm_views
 
 # Pydantic model for data validation
 class UserRegistrationRequest(BaseModel):
@@ -10,6 +13,10 @@ class UserRegistrationRequest(BaseModel):
     password: str
     phone: str
     address: str
+    role: str
+    grade: Optional[int] = None
+    board: Optional[str] = None
+    mentor_id: Optional[int] = None
 
 class UserLoginRequest(BaseModel):
     email: str
@@ -20,6 +27,15 @@ def register_user(request):
     if request.method == 'POST':
         registration_data = UserRegistrationRequest(**request.POST.dict())
         # Perform additional data validation if required
+        if len(registration_data.phone) != 10:
+            return JsonResponse({"error": "Invalid phone number"})
+        for char in registration_data.name:
+            if char.isdigit():
+                return JsonResponse({"error": "Name cannot contain numbers"})
+        if len(registration_data.password) < 8:
+            return JsonResponse({"error": "Password must be at least 8 characters long"})
+        if registration_data.email.find('@') == -1 or registration_data.email.find('.') == -1:
+            return JsonResponse({"error": "Invalid email"})
         user = User(
             Name=registration_data.name,
             Email=registration_data.email,
@@ -28,6 +44,14 @@ def register_user(request):
             Address=registration_data.address
         )
         user.save()
+        if registration_data.role == 'evaluator':
+            return eq_views.create_evaluator(request)
+        elif registration_data.role == 'qp_uploader':
+            return eq_views.create_qp_uploader(request)
+        elif registration_data.role == 'student':
+            return sm_views.create_student(request)
+        elif registration_data.role == 'mentor':
+            return sm_views.create_mentor(request)
         return JsonResponse({"message": "User registered successfully"})
     else:
         return JsonResponse({"error": "Invalid request method"})
